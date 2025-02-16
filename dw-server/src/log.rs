5ac4cefdd7d85b6febe65fa36d390c1e656ab913
Jan <jan@laupetin.net>
2025-02-16 13:17:34 +0100

    chore: log session id in custom logger config

---
+++ dw-server/src/log.rs
@@ -0,0 +1,118 @@
+﻿use bitdemon::networking::bd_session::SessionId;
+use bitdemon::networking::session_manager::SessionManager;
+use env_logger::fmt::{style, Formatter};
+use log::{LevelFilter, Record};
+use std::cell::Cell;
+use std::fmt::Display;
+use std::io;
+use std::io::Write;
+
+pub fn initialize_log() {
+    env_logger::builder()
+        .filter_level(LevelFilter::Info)
+        .format(move |buf, record| {
+            let fmt = CustomFormat {
+                written_header_value: false,
+                buf,
+            };
+
+            fmt.write(record)
+        })
+        .init();
+}
+
+#[derive(Copy, Clone)]
+struct SessionLogData {
+    pub session_id: SessionId,
+    pub session_type_name: &'static str,
+}
+
+impl Display for SessionLogData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}-{}", self.session_type_name, self.session_id)
+    }
+}
+
+thread_local! {
+    static SESSION_LOG_DATA: Cell<Option<SessionLogData>> = Cell::new(None);
+}
+
+pub fn log_session_id(session_manager: &SessionManager, session_type_name: &'static str) {
+    session_manager.on_session_registered(move |session| {
+        SESSION_LOG_DATA.set(Some(SessionLogData {
+            session_id: session.id,
+            session_type_name,
+        }))
+    })
+}
+
+struct CustomFormat<'a> {
+    written_header_value: bool,
+    buf: &'a mut Formatter,
+}
+
+// Based on Env Logger DefaultFormat
+impl CustomFormat<'_> {
+    fn write(mut self, record: &Record<'_>) -> io::Result<()> {
+        self.write_timestamp()?;
+        self.write_level(record)?;
+        self.write_target(record)?;
+        self.write_session()?;
+        self.finish_header()?;
+
+        self.write_args(record)?;
+        write!(self.buf, "\n")
+    }
+
+    fn write_header_value<T>(&mut self, value: T) -> io::Result<()>
+    where
+        T: Display,
+    {
+        if !self.written_header_value {
+            self.written_header_value = true;
+            let style = style::AnsiColor::BrightBlack.on_default();
+            write!(self.buf, "{style}[{style:#}{value}")
+        } else {
+            write!(self.buf, " {value}")
+        }
+    }
+
+    fn write_level(&mut self, record: &Record<'_>) -> io::Result<()> {
+        let level = record.level();
+        let level_style = self.buf.default_level_style(level);
+
+        self.write_header_value(format_args!("{level_style}{level:<5}{level_style:#}"))
+    }
+
+    fn write_timestamp(&mut self) -> io::Result<()> {
+        self.write_header_value(self.buf.timestamp_millis())
+    }
+
+    fn write_target(&mut self, record: &Record<'_>) -> io::Result<()> {
+        match record.target() {
+            "" => Ok(()),
+            target => self.write_header_value(target),
+        }
+    }
+
+    fn write_session(&mut self) -> io::Result<()> {
+        if let Some(session_log_data) = SESSION_LOG_DATA.get() {
+            self.write_header_value(session_log_data)
+        } else {
+            Ok(())
+        }
+    }
+
+    fn finish_header(&mut self) -> io::Result<()> {
+        if self.written_header_value {
+            let style = style::AnsiColor::BrightBlack.on_default();
+            write!(self.buf, "{style}]{style:#} ")
+        } else {
+            Ok(())
+        }
+    }
+
+    fn write_args(&mut self, record: &Record<'_>) -> io::Result<()> {
+        write!(self.buf, "{}", record.args())
+    }
+}
--- dw-server/src/main.rs
+++ dw-server/src/main.rs
@@ -1,27 +1,37 @@
 mod lobby;
+mod log;
 
 use crate::lobby::configure_lobby_server;
+use crate::log::{initialize_log, log_session_id};
 use bitdemon::auth::auth_server::AuthServer;
 use bitdemon::auth::key_store::InMemoryKeyStore;
 use bitdemon::lobby::LobbyServer;
 use bitdemon::networking::bd_socket::BdSocket;
-use log::LevelFilter;
+use bitdemon::networking::session_manager::SessionManager;
 use std::sync::Arc;
 
 const AUTH_SERVER_PORT: u16 = 3075;
 const LOBBY_SERVER_PORT: u16 = 3074;
 
 fn main() {
-    env_logger::builder().filter_level(LevelFilter::Info).init();
+    initialize_log();
 
-    let mut auth_socket = match BdSocket::new(AUTH_SERVER_PORT) {
-        Err(err) => {
-            panic!("Failed to open socket for auth server on port {AUTH_SERVER_PORT}: {err}")
-        }
-        Ok(s) => s,
-    };
+    let auth_session_manager = Arc::new(SessionManager::new());
+    log_session_id(auth_session_manager.as_ref(), "auth");
+    let mut auth_socket =
+        match BdSocket::new_with_session_manager(AUTH_SERVER_PORT, auth_session_manager) {
+            Err(err) => {
+                panic!("Failed to open socket for auth server on port {AUTH_SERVER_PORT}: {err}")
+            }
+            Ok(s) => s,
+        };
 
-    let mut lobby_socket = match BdSocket::new(LOBBY_SERVER_PORT) {
+    let lobby_session_manager = Arc::new(SessionManager::new());
+    log_session_id(lobby_session_manager.as_ref(), "lobby");
+    let mut lobby_socket = match BdSocket::new_with_session_manager(
+        LOBBY_SERVER_PORT,
+        lobby_session_manager.clone(),
+    ) {
         Err(err) => {
             panic!("Failed to open socket for lobby server on port {LOBBY_SERVER_PORT}: {err}")
         }
@@ -33,7 +43,7 @@ fn main() {
     let auth_server = Arc::new(AuthServer::new(key_store.clone()));
     let lobby_server = Arc::new(LobbyServer::new(key_store.clone()));
 
-    configure_lobby_server(&lobby_server);
+    configure_lobby_server(&lobby_server, lobby_session_manager);
 
     let auth_join = auth_socket.run_async(auth_server);
     let lobby_join = lobby_socket.run_async(lobby_server);
