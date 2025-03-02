d2f1dab6a7e03651c6340349f2fa014dfb5c4e4d
Jan <jan@laupetin.net>
2025-03-02 23:06:43 +0100

    feat: serve publisher streams via http

--- dw-server/src/lobby/content_streaming/http.rs
+++ dw-server/src/lobby/content_streaming/http.rs
@@ -1,9 +1,16 @@
 use crate::lobby::content_streaming::publisher_file::DwPublisherContentStreamingService;
 use axum::extract::{Path, State};
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
 use axum::routing::get;
 use axum::Router;
+use axum_extra::response::FileStream;
+use bitdemon::domain::title::Title;
 use log::info;
+use num_traits::FromPrimitive;
 use std::sync::Arc;
+use tokio::fs::File;
+use tokio_util::io::ReaderStream;
 
 pub fn create_content_streaming_router(
     publisher_service: Arc<DwPublisherContentStreamingService>,
@@ -19,6 +26,23 @@ pub fn create_content_streaming_router(
 async fn retrieve_publisher_file(
     Path((title_num, file_id)): Path<(u32, u64)>,
     State(publisher_service): State<Arc<DwPublisherContentStreamingService>>,
-) {
-    info!("Retrieving publisher file for {title_num} and {file_id}")
+) -> Result<Response, (StatusCode, String)> {
+    info!("Streaming publisher file for {title_num} and {file_id}");
+
+    let title = Title::from_u32(title_num)
+        .ok_or_else(|| (StatusCode::BAD_REQUEST, "Illegal title num".to_string()))?;
+
+    let stream = publisher_service
+        .stream_by_id(title, file_id)
+        .ok_or_else(|| (StatusCode::NOT_FOUND, "Stream not found".to_string()))?;
+
+    let file_name = format!("stream/publisher/{title_num}/{}", stream.filename);
+    let file = File::open(file_name.as_str())
+        .await
+        .map_err(|e| (StatusCode::NOT_FOUND, format!("File not found: {e}")))?;
+
+    let stream = ReaderStream::new(file);
+    let file_stream_resp = FileStream::new(stream).file_name(file_name);
+
+    Ok(file_stream_resp.into_response())
 }
--- dw-server/src/lobby/content_streaming/publisher_file.rs
+++ dw-server/src/lobby/content_streaming/publisher_file.rs
@@ -1,10 +1,22 @@
 ﻿use bitdemon::domain::result_slice::ResultSlice;
+use bitdemon::domain::title::Title;
 use bitdemon::lobby::content_streaming::{
     ContentStreamingServiceError, PublisherContentStreamingService, StreamInfo,
 };
 use bitdemon::networking::bd_session::BdSession;
+use chrono::{DateTime, Utc};
+use log::info;
+use num_traits::ToPrimitive;
+use std::collections::HashMap;
+use std::fs;
+use std::fs::DirEntry;
+use std::ops::Sub;
+use std::sync::{RwLock, RwLockReadGuard};
+use std::time::UNIX_EPOCH;
 
-pub struct DwPublisherContentStreamingService {}
+pub struct DwPublisherContentStreamingService {
+    publisher_streams: RwLock<HashMap<Title, PublisherStreamState>>,
+}
 
 impl PublisherContentStreamingService for DwPublisherContentStreamingService {
     fn get_publisher_stream_by_id(
@@ -12,7 +24,14 @@ impl PublisherContentStreamingService for DwPublisherContentStreamingService {
         session: &BdSession,
         file_id: u64,
     ) -> Result<StreamInfo, ContentStreamingServiceError> {
-        todo!()
+        info!("Getting publisher stream {file_id}");
+
+        let authentication = session
+            .authentication()
+            .expect("authentication was required for handler");
+
+        self.stream_by_id(authentication.title, file_id)
+            .ok_or(ContentStreamingServiceError::NoStreamFound)
     }
 
     fn list_publisher_streams(
@@ -23,7 +42,33 @@ impl PublisherContentStreamingService for DwPublisherContentStreamingService {
         item_offset: usize,
         item_count: usize,
     ) -> Result<ResultSlice<StreamInfo>, ContentStreamingServiceError> {
-        todo!()
+        info!("Listing publisher streams min={min_date_time} category={category} offset={item_offset} count={item_count}");
+
+        let authentication = session
+            .authentication()
+            .expect("authentication was required for handler");
+
+        let read = self.read_publisher_streams(authentication.title);
+
+        let state = read
+            .get(&authentication.title)
+            .expect("state to be created");
+
+        // TODO: Filter for category
+        let stream_info: Vec<StreamInfo> = state
+            .streams
+            .iter()
+            .filter(|info| info.modified >= min_date_time)
+            .skip(item_offset)
+            .take(item_count)
+            .cloned()
+            .collect();
+
+        if !stream_info.is_empty() {
+            Ok(ResultSlice::new(stream_info, item_offset))
+        } else {
+            Err(ContentStreamingServiceError::NoStreamFound)
+        }
     }
 
     fn filter_publisher_streams(
@@ -35,12 +80,176 @@ impl PublisherContentStreamingService for DwPublisherContentStreamingService {
         item_count: usize,
         filter: String,
     ) -> Result<ResultSlice<StreamInfo>, ContentStreamingServiceError> {
-        todo!()
+        info!("Filtering publisher streams filter={filter} min={min_date_time} category={category} offset={item_offset} count={item_count}");
+
+        let authentication = session
+            .authentication()
+            .expect("authentication was required for handler");
+
+        let read = self.read_publisher_streams(authentication.title);
+
+        let state = read
+            .get(&authentication.title)
+            .expect("state to be created");
+
+        // TODO: Filter for category
+        let stream_info: Vec<StreamInfo> = state
+            .streams
+            .iter()
+            .filter(|info| info.modified >= min_date_time)
+            .filter(|info| info.filename.starts_with(&filter))
+            .skip(item_offset)
+            .take(item_count)
+            .cloned()
+            .collect();
+
+        if !stream_info.is_empty() {
+            Ok(ResultSlice::new(stream_info, item_offset))
+        } else {
+            Err(ContentStreamingServiceError::NoStreamFound)
+        }
     }
 }
 
 impl DwPublisherContentStreamingService {
     pub fn new() -> DwPublisherContentStreamingService {
-        DwPublisherContentStreamingService {}
+        let state_map = HashMap::new();
+
+        DwPublisherContentStreamingService {
+            publisher_streams: RwLock::new(state_map),
+        }
+    }
+
+    pub fn stream_by_id(&self, title: Title, file_id: u64) -> Option<StreamInfo> {
+        let lock = self.read_publisher_streams(title);
+        let state = lock.get(&title).expect("state to be created");
+
+        state
+            .streams
+            .iter()
+            .find(|info| info.id == file_id)
+            .cloned()
+    }
+
+    fn read_publisher_streams(
+        &self,
+        title: Title,
+    ) -> RwLockReadGuard<'_, HashMap<Title, PublisherStreamState>> {
+        {
+            let lock = self.publisher_streams.read().unwrap();
+            if let Some(stream_state) = lock.get(&title) {
+                if !stream_state.refresh_necessary() {
+                    return lock;
+                }
+            }
+        }
+
+        {
+            let mut lock = self.publisher_streams.write().unwrap();
+            if let Some(write_state) = lock.get_mut(&title) {
+                write_state.refresh_if_necessary();
+            } else {
+                lock.insert(title, PublisherStreamState::create_and_initialize(title));
+            }
+        }
+
+        let lock = self.publisher_streams.read().unwrap();
+        lock
+    }
+}
+
+struct PublisherStreamState {
+    last_refresh: DateTime<Utc>,
+    title: Title,
+    next_id: u64,
+    streams: Vec<StreamInfo>,
+}
+
+const STATE_REFRESH_SECONDS: i64 = 60;
+
+impl PublisherStreamState {
+    fn create_and_initialize(title: Title) -> Self {
+        let mut result = PublisherStreamState {
+            last_refresh: Utc::now(),
+            title,
+            next_id: 1,
+            streams: Vec::new(),
+        };
+
+        result.refresh();
+
+        result
+    }
+
+    fn refresh_necessary(&self) -> bool {
+        let now = Utc::now();
+
+        now.sub(self.last_refresh).num_seconds() > STATE_REFRESH_SECONDS
+    }
+
+    fn refresh_if_necessary(&mut self) {
+        if self.refresh_necessary() {
+            self.refresh();
+        }
+    }
+
+    fn refresh(&mut self) {
+        let dir_name = format!("stream/publisher/{}", self.title.to_u32().unwrap());
+        if let Ok(dir) = fs::read_dir(dir_name) {
+            dir.filter_map(|entry| entry.ok())
+                .for_each(|entry| self.handle_entry(entry));
+        }
+    }
+
+    fn handle_entry(&mut self, entry: DirEntry) {
+        let metadata = entry.metadata().expect("metadata to be retrievable");
+        let filename = entry.file_name().into_string().unwrap();
+
+        let maybe_existing_entry = self
+            .streams
+            .iter_mut()
+            .find(|stream| stream.filename == filename);
+
+        if let Some(existing_entry) = maybe_existing_entry {
+            existing_entry.stream_size = metadata.len();
+            existing_entry.modified = metadata
+                .modified()
+                .unwrap()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs() as i64;
+        } else {
+            let id = self.next_id;
+            let title_num = self.title.to_u32().unwrap();
+            self.next_id += 1;
+            self.streams.push(StreamInfo {
+                id,
+                filename: entry.file_name().into_string().unwrap(),
+                title: self.title,
+                stream_size: metadata.len(),
+                created: metadata
+                    .created()
+                    .unwrap()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs() as i64,
+                modified: metadata
+                    .modified()
+                    .unwrap()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs() as i64,
+                owner_id: 0,
+                owner_name: "".to_string(),
+                url: format!("http://localhost:3000/content/publisher/{title_num}/{id}"),
+                metadata: vec![],
+                category: 0,
+                slot: 0,
+                tags: vec![],
+                num_copies_made: 0,
+                summary_file_size: 0,
+                origin_id: 0,
+            });
+        }
     }
 }
